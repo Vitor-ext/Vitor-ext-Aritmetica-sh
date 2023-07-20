3e8f406348798694bf610a0733c126e32ed2a41e
VITOR SANTANA DE JESUS <sn1079379@cfp127aluno.local>
2023-07-20 14:23:36 -0300

    Exemplo

---
+++ script.sh
@@ -0,0 +1,15 @@
+let "SUB=15-15"
+printf "%d" "$SUB"
+sleep 5
+
+let "SOMA=15+15"
+printf "%d" "$SOMA"
+sleep 5
+
+let "DIVIDI=20/2"
+printf "%d" "$DIVIDI"
+
+sleep 5
+
+let "MULTI=5*3"
+printf "%d" "$MULTI"
